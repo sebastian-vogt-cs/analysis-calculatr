820c53e5d64bf26611be5984d0ef0053520c709c
sebastian <vogt999@gmail.com>
2018-02-01 21:51:31 +0100

    add possibility to use floating point factors like "10.3x²"

--- src/fmath.rs
+++ src/fmath.rs
@@ -1,10 +1,10 @@
-pub fn derive(func:&Vec<(i8, u32, u32)>)->Vec<(i8, u32, u32)>{
-    let mut new_func:Vec<(i8, u32, u32)> = Vec::new();
-    let mut memory:(i8, u32, u32) = (1, 0, 0);
+pub fn derive(func:&Vec<(i8, f64, usize)>)->Vec<(i8, f64, usize)>{
+    let mut new_func:Vec<(i8, f64, usize)> = Vec::new();
+    let mut memory:(i8, f64, usize) = (1, 0.0, 0);
     for &(sign, a, n) in func{
         if n >= 1{
             memory.2 = n - 1;
-            memory.1 = a * n;
+            memory.1 = a * (n as f64);
             memory.0 = sign;
             new_func.push(memory);
         }
--- src/function_parser.rs
+++ src/function_parser.rs
@@ -1,76 +1,93 @@
 //this func checks weather or not the function is a correctly entered polynomal function and maps the function in a usable way into a Vec
 //the resulting Vec consist of a tuple for each term, the tuple consists of the multiplicant a and the power n (ax^n)
 //the parser is basically a deterministic finite state machine, draw it up in a diagramm to understand it.
-pub fn parse_function(input:&str) -> Vec<(i8, u32, u32)>{
+pub fn parse_function(input:&str) -> Vec<(i8, f64, usize)>{
     let mut state:u8 = 0;
     let function:&str = &input[7..input.len()];
-    let mut polynomal_representation:Vec<(i8, u32, u32)> = Vec::new();
-    let mut memory:(i8, u32, u32) = (1, 0, 0);
+    let mut polynomal_representation:Vec<(i8, f64, usize)> = Vec::new();
+    let mut memory:(i8, f64, usize) = (1, 0.0, 0);
+    let mut dezplace:usize = 10;
 
     for c in function.chars() {
         match state {
             0 =>{
                 match c {
-                    '-' => {state = 6; memory = (-1, 1, 0)},
-                    'x' => {state = 2; memory = (memory.0, 1, 1)},
-                    '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {state = 3; memory = (memory.0, c.to_digit(10).unwrap() as u32, 0)},
-                    _ => {state = 7; break;},
+                    '-' => {state = 6; memory = (-1, 1.0, 0)},
+                    'x' => {state = 2; memory = (memory.0, 1.0, 1)},
+                    '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {state = 3; memory = (memory.0, c.to_digit(10).unwrap() as f64, 0)},
+                    _ => {state = 8; break;},
                 }
             }
             1 =>{
                 match c {
-                    'x' => {state = 2; memory = (memory.0, 1, 1)},
-                    '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {state = 3; memory = (memory.0, c.to_digit(10).unwrap() as u32, 0)},
-                    _ => {state = 7; break;},
+                    'x' => {state = 2; memory = (memory.0, 1.0, 1)},
+                    '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => {state = 3; memory = (memory.0, c.to_digit(10).unwrap() as f64, 0)},
+                    _ => {state = 8; break;},
                 }
             }
             2 =>{
                 match c {
                     ' ' => {state = 5; memory = (memory.0, memory.1, 1); polynomal_representation.push(memory)},
-                    '¹' | '²' | '³' | '⁴' | '⁵' | '⁶' | '⁷' | '⁸' | '⁹' => {state = 4; memory = (memory.0, memory.1, superscript_to_u32(&c))},
-                    _ => {state = 7; break;},
+                    '¹' | '²' | '³' | '⁴' | '⁵' | '⁶' | '⁷' | '⁸' | '⁹' => {state = 4; memory = (memory.0, memory.1, superscript_to_usize(&c))},
+                    _ => {state = 8; break;},
                 }
             }
             3 =>{
                 match c {
                     ' ' => {state = 5; memory = (memory.0, memory.1, 0); polynomal_representation.push(memory)},
                     'x' => {state = 2; memory = (memory.0, memory.1, 1);}
-                    '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '0' => {state = 3; memory = (memory.0, memory.1*10 + c.to_digit(10).unwrap() as u32, 0)},
-                    _ => {state = 7; break;},
+                    '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '0' => {state = 3; 
+                        memory = (memory.0, memory.1*10.0 + c.to_digit(10).unwrap() as f64, 0);
+                    },
+                    '.' => state = 7,
+                    _ => {state = 8; break;},
                 }
             }
             4 =>{
                 match c {
-                    '¹' | '²' | '³' | '⁴' | '⁵' | '⁶' | '⁷' | '⁸' | '⁹' | '⁰' => {state = 4; memory = (memory.0, memory.1, memory.2*10 + superscript_to_u32(&c))},
+                    '¹' | '²' | '³' | '⁴' | '⁵' | '⁶' | '⁷' | '⁸' | '⁹' | '⁰' => {state = 4; 
+                        memory = (memory.0, memory.1, memory.2*10 + superscript_to_usize(&c));
+                    },
                     ' ' => {state = 5; memory = (memory.0, memory.1, memory.2); polynomal_representation.push(memory)},
-                    _ => {state = 7; break;},
+                    _ => {state = 8; break;},
                 }
             }
             5 =>{
                 match c {
-                    '+' => {state = 6; memory = (1, 0, 0)},
-                    '-' => {state = 6; memory = (-1, 0, 0)},
-                    _ => {state = 7; break;},
+                    '+' => {state = 6; memory = (1, 0.0, 0)},
+                    '-' => {state = 6; memory = (-1, 0.0, 0)},
+                    _ => {state = 8; break;},
                 }
             }
             6 =>{
                 match c {
                     ' ' => state = 0,
-                    _ => {state = 7; break;},
+                    _ => {state = 8; break;},
                 }
             }
-            _ => {state = 7; break;},
+            7 =>{
+                match c{
+                    ' ' => {state = 5; memory = (memory.0, memory.1, 0); polynomal_representation.push(memory)},
+                    'x' => {state = 2; memory = (memory.0, memory.1, 1);}
+                    '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '0' => {
+                        state = 7; 
+                        memory = (memory.0, memory.1 + (c.to_digit(10).unwrap() as f64)/(dezplace as f64), 0); dezplace = dezplace * 10;
+                    },
+                    _ => {state = 8; break;},
+                }
+            }
+            _ => {state = 8; break;},
         }
     }
     polynomal_representation.push(memory);
-    if (state == 2) | (state == 3) | (state == 4) {
+    if (state == 2) | (state == 3) | (state == 4) | (state == 7) {
         polynomal_representation
     }else{
         Vec::new()
     }
 }
 
-pub fn func_to_string(func:&Vec<(i8, u32, u32)>)->String{
+pub fn func_to_string(func:&Vec<(i8, f64, usize)>)->String{
     let mut func_string:String = String::new();
     for i in 0 .. (func.len()) {
         if func[i].0 != 1 {
@@ -80,7 +97,7 @@ pub fn func_to_string(func:&Vec<(i8, u32, u32)>)->String{
         }
         func_string.push_str(&func[i].1.to_string());
         func_string.push('x');
-        func_string.push_str(&u32_to_superscript(func[i].2));
+        func_string.push_str(&usize_to_superscript(func[i].2));
     }
     func_string
 }
@@ -88,7 +105,7 @@ pub fn func_to_string(func:&Vec<(i8, u32, u32)>)->String{
 
 
 //this function converts number like ¹²³³⁴⁵ to normal u32
-fn superscript_to_u32(char:&char)->u32{
+fn superscript_to_usize(char:&char)->usize{
     match *char as u16{
         185  => 1,
         178  => 2,
@@ -104,7 +121,7 @@ fn superscript_to_u32(char:&char)->u32{
     }
 }
 
-fn u32_to_superscript(num:u32)->String{
+fn usize_to_superscript(num:usize)->String{
     let nums:String = num.to_string();
     let mut result:String = String::new();
     for c in nums.chars(){
