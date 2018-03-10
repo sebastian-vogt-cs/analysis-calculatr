3f89a7a684c101923b808e286f3440fe9b0db404
Sebastian <vogt999@gmail.com>
2018-03-10 11:00:15 +0100

    Improve the mathmatics behind the zeros calculator. The number of iterations in Newton's method is now dependent on the function itsself. For extremely low a's and high n's it's still not optimized (smth like 0.0000001x¹⁰⁰)

--- src/fmath.rs
+++ src/fmath.rs
@@ -36,26 +36,26 @@ pub fn get_y_for(x:f64, func:&Vec<(bool, f64, usize)>)->f64{
 
 
 //calculate zeros of a function via newton algorithm
-pub fn get_zeros(func:&Vec<(bool, f64, usize)>)->Vec<f64>{
+pub fn get_zeros(func:&Vec<(bool, f64, usize)>, iterations:u16)->Vec<f64>{
 
     //this variable stores the zeros
     let mut values:Vec<f64> = Vec::new();
 
     //if it is a linear function run newton_alg and return the value
     if func[0].2 == 1{
-        values.push(round(newton_alg(0.0, func)));
+        values.push(round(newton_alg(0.0, func, iterations)));
         return values;
 
     //else get the extrema (zeros of derivative -> recursion until derivative is x¹).
     }else{
         
         //if two neighboring extrema have different signs search for a zeros in between via newton. Loop through all the extrema for that.
-        let extrema = get_zeros(&derive(func));
+        let extrema = get_zeros(&derive(func), iterations + 1);
         let last_extreme:f64 = extrema[extrema.len() - 1];
         let mut memory:f64 = extrema[0] - 1.0;
         for x in extrema{
             if (get_y_for(x, func).is_sign_positive() && !(get_y_for(memory, func).is_sign_positive())) || (!(get_y_for(x, func).is_sign_positive()) && get_y_for(memory, func).is_sign_positive()) || (get_y_for(memory, func) == 0.0) || (get_y_for(x, func) == 0.0){
-                let new_zero = round(newton_alg((x + memory) / 2.0, func));
+                let new_zero = round(newton_alg((x + memory) / 2.0, func, iterations));
 
                 //check if the new zero isn't already there
                 if ((values.len() >= 1) && (new_zero != values[values.len()-1])) || (values.len() == 0){
@@ -66,7 +66,7 @@ pub fn get_zeros(func:&Vec<(bool, f64, usize)>)->Vec<f64>{
         }
 
         //redo that process for the space after the last extreme
-        let new_zero = round(newton_alg(last_extreme + 1.0, func));
+        let new_zero = round(newton_alg(last_extreme + 1.0, func, iterations));
         if ((values.len() >= 1) && (new_zero != values[values.len()-1])) || (values.len() == 0){
                     values.push(new_zero);
          }
@@ -76,10 +76,10 @@ pub fn get_zeros(func:&Vec<(bool, f64, usize)>)->Vec<f64>{
 
 
 //implementation of the newton algorithm for calculating zeros. Google if you want to know how the maths work
-fn newton_alg(x_start:f64, func:&Vec<(bool, f64, usize)>)->f64{
+fn newton_alg(x_start:f64, func:&Vec<(bool, f64, usize)>, iterations:u16)->f64{
     let mut x:f64 = x_start;
     let derivative:Vec<(bool, f64, usize)> = derive(func);
-    for _i in 0..10000{
+    for _i in 0..(((func[0].2 as f64 + iterations as f64).powf(2.0))/func[0].1.sqrt()) as usize{
         x = x - (get_y_for(x, func)/get_y_for(x, &derivative));
     }
     x
