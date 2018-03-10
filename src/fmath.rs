8c73709b9897af246232ec90f4e2ec851c21236a
Sebastian <vogt999@gmail.com>
2018-03-10 20:24:06 +0100

    Zeros algorithm now is beta! Newton's method now stops when it's found a zero (taking accuracy on account). The only remaining weakness are zeros of "high degree", eg. the zero of x¹⁰⁰ which would be calculated as 0.8 or something

--- src/fmath.rs
+++ src/fmath.rs
@@ -52,7 +52,9 @@ pub fn get_zeros(func:&Vec<(bool, f64, usize)>)->Vec<f64>{
         //if two neighboring extrema have different signs search for a zeros in between via newton. Loop through all the extrema for that.
         let extrema = get_zeros(&derive(func));
         let last_extreme:f64 = extrema[extrema.len() - 1];
-        let mut memory:f64 = extrema[0] - 1.0;
+        let mut memory:f64 = extrema[0];
+        let new_zero = round(newton_alg(memory - 1.0, func));
+        values.push(new_zero);
         for x in extrema{
             if (get_y_for(x, func).is_sign_positive() && !(get_y_for(memory, func).is_sign_positive())) || (!(get_y_for(x, func).is_sign_positive()) && get_y_for(memory, func).is_sign_positive()) || (get_y_for(memory, func) == 0.0) || (get_y_for(x, func) == 0.0){
                 let new_zero = round(newton_alg((x + memory) / 2.0, func));
@@ -79,7 +81,10 @@ pub fn get_zeros(func:&Vec<(bool, f64, usize)>)->Vec<f64>{
 fn newton_alg(x_start:f64, func:&Vec<(bool, f64, usize)>)->f64{
     let mut x:f64 = x_start;
     let derivative:Vec<(bool, f64, usize)> = derive(func);
-    for _i in 0..(((func[0].2 as f64).powf(2.0))/func[0].1.sqrt()) as usize{
+    loop{
+        if round(get_y_for(round(x), func)) == 0.0{
+            break
+        }
         x = x - (get_y_for(x, func)/get_y_for(x, &derivative));
     }
     x
@@ -88,9 +93,5 @@ fn newton_alg(x_start:f64, func:&Vec<(bool, f64, usize)>)->f64{
 
 //round f64 to ACCURACY, which is a static global variable
 fn round(num:f64)->f64{
-    if (((1.0/ACCURACY) > num) && (num.is_sign_positive())) || ((-(1.0/ACCURACY) < num) && (!num.is_sign_positive())){
-        0.0
-    }else{
-        (num * ACCURACY).round()/ACCURACY
-    }
+    (num * ACCURACY).round()/ACCURACY
 }
